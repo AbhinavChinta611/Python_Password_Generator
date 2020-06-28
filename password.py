817ac2ec9e856fc7555470346fe9cb169ce7ca0b
Abhinav Chinta <66627012+AbhinavChinta611@users.noreply.github.com>
2020-06-28 21:44:27 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,14 @@
+import random
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{}<>();'/.,~!@#$%^&*"
+
+all = lower+upper+numbers+symbols
+
+length = int(input("Enter password length : "))
+#uncommnet the next line to set length to 8 characters
+#length = 8
+
+password = "".join(random.sample(all,length))
+print(password)
